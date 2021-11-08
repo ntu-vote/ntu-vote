9b1cd91e651560b3b775862ffe59d0cd5dc5e07b
Ming-HanLee <47711448+Ming-HanLee@users.noreply.github.com>
2021-11-08 21:42:44 +0800

    Fixed register / login apis

--- middleware/authorization.ts
+++ middleware/authorization.ts
@@ -56,7 +56,7 @@ export const authMiddleware = async (ctx: Koa.ParameterizedContext, next: Next) 
 
 // generate email verify token
 export const generateAccessToken = (payload) => {
-  return jwt.sign(payload, process.env.SECRET, {
+  return jwt.sign(payload, '_:d//`2a?E2B^s~p&`XMhGT[7~4w)atTMY]:F3HPn{6c2+XFDC6-:R8%Z)w@GH/f"E-^ds>zb*<x4yev3Lq%c+.qZ?8.H`JjLRtYXf-~,Vqc46-yk+"VXE)w<WT:48$FNG/p4^YQ%vgWKUN7Y$2G9hC;):r5`M=Y?`Db$hZz8-Fd[jqNncFm4yNz8Wa#+E#b;k/]<\'e3}SSu8BwCnB$D!&ZNX~_rGF\'}FKDvR/R+/3D\\SdzA[6\\VTMUJ9H]X3T@/', {
       expiresIn: "1800s",
   });
 };
--- service/login.ts
+++ service/login.ts
@@ -4,6 +4,6 @@ import { getRepository } from 'typeorm'
 export const searchVoterAccount = async (username: string) => {
     return await getRepository(VoterAccount).findOne({ 
         where: { username: username },
-        relations: ['uid', 'pub_id', 'prv_id']
+        relations: ['info']
      });
 }
